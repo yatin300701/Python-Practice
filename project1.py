dfa2d2b988f1897996e2deda2e1ffaf9fca56410
Yatin Choudhary <75214427+yatin300701@users.noreply.github.com>
2022-06-12 18:05:46 +0530

    read and write commands

---
+++ project1.py
@@ -0,0 +1,13 @@
+# #######  Project 1 ####### #
+import time
+fs = open("project1.txt", "r+")
+print("\n"+fs.readline())
+a = fs.readline()
+print(a)
+waterLog = int(a[15])
+fs.read()
+
+print(t)
+fs.writable()
+
+fs.close()
